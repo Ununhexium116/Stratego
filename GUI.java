cc0c5d10c2db1437d23e4bc4e455f20f3db6fda2
Ununhexium116 <ununhaxium@gmail.com>
2017-11-13 11:29:31 -0500

    13/11/2017 Commit
    Test commit to push code to github

--- GUI.java
+++ GUI.java
@@ -1,44 +1,54 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class GUI {
+public class GUI implements ActionListener{
 
-    JFrame window = new JFrame("Noah's Laptop Gaeey");
+    JFrame window = new JFrame("Jared mad bad");
     CardLayout cl = new CardLayout();
-    JPanel startScreen = new JPanel(new BorderLayout());
+    JPanel startScreen = new JPanel(new GridLayout(2,1));
     JPanel gameScreen = new JPanel();
     JPanel contPanel = new JPanel(cl);
 
     JLabel title = new JLabel();
     JButton startButton = new JButton("Start");
 
-    ImageIcon titleImage = new ImageIcon("Desktop//Kids Camp Week 3 Camp Photo//stratego.png");
+    ImageIcon titleImage = new ImageIcon("src/src/stratego.png");
 
     public GUI(){
         Font();
         window.add(contPanel);
         window.setVisible(true);
-        window.setSize(500,500);
+        window.setSize(1000,500);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        startButton.addActionListener(this);
 
-        contPanel.add(startScreen, "S1");
-        contPanel.add(gameScreen, "S2");
+        contPanel.add(startScreen, "start");
+        contPanel.add(gameScreen, "game");
 
         startScreen.add(title, BorderLayout.NORTH);
         startScreen.add(startButton, BorderLayout.SOUTH);
 
-        cl.show(contPanel,"S1");
+        gameScreen.setPreferredSize(new Dimension(1000,1000));
+
+        cl.show(contPanel,"start");
     }
     public void Font(){
         title.setForeground(Color.RED);
-        title.setFont(new Font("Serif", Font.PLAIN, 36));
+        title.setFont(new Font("Helvetica", Font.PLAIN, 36));
         title.setBackground(Color.GRAY);
         title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setIcon(titleImage);
         startButton.setForeground(Color.CYAN);
-        startButton.setFont(new Font("Serif", Font.PLAIN, 36));
+        startButton.setFont(new Font("Helvetica", Font.PLAIN, 36));
         startButton.setBackground(Color.GRAY);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startButton) {
+            cl.show(contPanel, "game");
+        }
+    }
 }
