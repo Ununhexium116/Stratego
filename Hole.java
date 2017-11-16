df31bcaf4975838e85376f2ab2a06d4cfe54e1bc
Ununhexium116 <ununhaxium@gmail.com>
2017-11-16 13:43:25 -0500

    remove getter and setter from Hole.java

--- Hole.java
+++ Hole.java
@@ -2,18 +2,7 @@ public class Hole extends Piece{
 
     boolean hole = true;
 
-    public Hole(int startX, int startY, int startStregnth, boolean startHole) {
+    public Hole(int startX, int startY, int startStregnth) {
         super(startX, startY, startStregnth);
-        this.hole = startHole;
-    }
-
-    // Setter
-    public void setHole(boolean holeInput) {
-        this.hole = holeInput;
-    }
-
-    // Getter
-    public boolean getHole() {
-        return this.hole;
     }
 }
